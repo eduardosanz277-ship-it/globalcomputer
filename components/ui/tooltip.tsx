de86571e8471a7bd7629c0be4e54a9ca29c36f6c
Victor Cabrera <eduardosanz277@gmail.com>
2026-03-23 15:41:17 -0400

    Add admin UI tooltip component

---
+++ components/ui/tooltip.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { Arrow as TooltipArrowPrimitive } from "@radix-ui/react-tooltip";
+import { cn } from "@/utils/cn";
+
+const TooltipProvider = TooltipPrimitive.Provider;
+
+const Tooltip = TooltipPrimitive.Root;
+
+const TooltipTrigger = TooltipPrimitive.Trigger;
+
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 8, children, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-[200] max-w-xs rounded-md border border-zinc-700/70 bg-zinc-900 px-3 py-1.5 text-xs leading-snug text-zinc-50 shadow-lg",
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <TooltipArrowPrimitive
+        aria-hidden
+        className="fill-zinc-900"
+        height={7}
+        width={14}
+      />
+    </TooltipPrimitive.Content>
+  </TooltipPrimitive.Portal>
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
